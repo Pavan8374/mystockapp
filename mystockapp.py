099d824f4391788e700635e498c7778ab3352621
Pavan Kumar <97884852+Pavan8374@users.noreply.github.com>
2022-11-10 09:07:11 +0530

    Update mystockapp.py

--- mystockapp.py
+++ mystockapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from datetime import date
 
-import yfinance as yf
+import yfinance 
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly
 from plotly import graph_objs as go
